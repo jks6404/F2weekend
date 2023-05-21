6beae5e2e76510c43873457ad2a004e57ecdd556
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-21 22:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,87 @@
-let database = [{id:1,name:"john",age:"18",profession:"developer"},
-                {id:2, name:"jack",age:"20", profession:"developer"},
-                {id:3, name:"karen", age:"19",profession:"admin"}]
-let temp = document.getElementById("insert");
-function display(){
-    for(let i=0;i<database.length;i++){
-    let elements = document.createElement("div");
-    elements.className = "insertedNames";
-    elements.innerText = "ID: "+database[i].id + " Name: "+database[i].name  + " Profession: "+database[i].profession +" Age: "+ database[i].age;
-    temp.append(elements)
+let users=[{id:1,name:"john",age:"18",profession:"developer"},
+{id:2, name:"jack",age:"20", profession:"developer"},
+{id:3, name:"karen", age:"19",profession:"admin"}];
+let div=document.getElementById("table");
+for(let i=0;i<users.length;i++){
+    const user=document.createElement("div");
+    user.className="user";
+    const id=document.createElement("div");
+    id.innerText=users[i].id+".";
+    const name=document.createElement("div");
+    name.innerText="Name: "+users[i].name;
+    const age=document.createElement("div");
+    age.innerText="Age: "+users[i].age;
+    const profession=document.createElement("div");
+    profession.innerText="Profession: "+users[i].profession;
+
+    user.append(id);
+    user.append(name);
+    user.append(profession);
+    user.append(age);
+    div.append(user)
+}
+
+function myFilter(){
+    let select=document.getElementById("profession").value;
+    if(select==="none"){
+        alert("Please select a profession before clicking the button");
+        return;
     }
+  const newSelect=select.toLowerCase();
+  const filteredArr = users.filter(ele => {
+    return ele.profession===newSelect;
+  });
+//   hide elements
+  const user=document.getElementsByClassName("user");
+  for(let i=0;i<user.length;i++){
+    user[i].style.display="none";
+  }
+
+//   Show the filtered rows
+  filteredArr.forEach((element) => {
+    const user=document.createElement("div");
+    user.className="user";
+    const id=document.createElement("div");
+    id.innerText=element.id+".";
+    const name=document.createElement("div");
+    name.innerText="Name: "+element.name;
+    const age=document.createElement("div");
+    age.innerText="Age: "+element.age;
+    const profession=document.createElement("div");
+    profession.innerText="Profession: "+element.profession;
+
+    user.append(id);
+    user.append(name);
+    user.append(profession);
+    user.append(age);
+    div.append(user)
+  });
 }
 
-display();
-let masterIndex = 4;
-function updateDataBase(){
-    let values = document.getElementsByTagName("input");
-    let obj = {id:"",name:"",age:"",profession:""};
-    obj.id = masterIndex++;
-    obj.name = values[0].value;
-    obj.profession = values[1].value;
-    obj.age = values[2].value;
-    database.push(obj);
 
 
-        let elements = document.createElement("div");
-        elements.className = "insertedNames";
-        elements.innerText = "ID: "+masterIndex + " Name: "+obj.name  + " Profession: "+obj.profession +" Age: "+ obj.age;
-        temp.append(elements);
-    
-    console.log(database);
 
+function addUser(){
+    let userObj={};
+    userObj.id=users.length+1;
+    userObj.name=document.getElementById("fullName").value;
+    userObj.profession=document.getElementById("prof").value.toLowerCase();
+    userObj.age=document.getElementById("age").value;
+    users.push(userObj);
+    const user=document.createElement("div");
+    user.className="user";
+    const id=document.createElement("div");
+    id.innerText=users[users.length-1].id+".";
+    const name=document.createElement("div");
+    name.innerText="Name: "+users[users.length-1].name;
+    const age=document.createElement("div");
+    age.innerText="Age: "+users[users.length-1].age;
+    const profession=document.createElement("div");
+    profession.innerText="Profession: "+users[users.length-1].profession;
 
-}
-
-let forSelect = document.getElementById("forProfession");
-console.log(forSelect)
-function filter(){
-    let output = database.filter(function(element,index,database){
-        return element.profession=="developer";
-    })
-
-    
-    console.log(output);
+    user.append(id);
+    user.append(name);
+    user.append(profession);
+    user.append(age);
+    div.append(user)
 }
